334d19cfaf61e8bccf7c6791130481daa114ed37
Krish Garg <65218584+KrishGarg@users.noreply.github.com>
2022-02-24 01:12:32 +0000

    switched to replit from deta as deta didn't allow ip access to me

--- index.js
+++ index.js
@@ -13,17 +13,15 @@ app.get("/", function (req, res) {
 });
 
 app.get("/api/whoami", function (req, res) {
-  console.log(req.ip);
   return res.send({
-    ...req.headers,
-    ip: req.ip,
+    ipaddress: req.clientIp,
     software: req.headers["user-agent"],
     language: req.headers["accept-language"],
   });
 });
 
 if (!process.env.DETA_RUNTIME) {
-  const listener = app.listen(process.env.PORT ?? 3000, function () {
+  const listener = app.listen(process.env.PORT || 3000, function () {
     console.log(
       "Your app is listening on http://localhost:" + listener.address().port
     );
