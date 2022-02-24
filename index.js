279b77545f114535c7efcaf2dea9ec192c001289
Krish Garg <krishgarg6306@gmail.com>
2022-02-24 06:34:11 +0530

    s

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const requestIp = require("request-ip");
+const app = express();
+
+const cors = require("cors");
+app.use(cors({ optionsSuccessStatus: 200 }));
+
+app.use(express.static("public"));
+app.use(requestIp.mw());
+
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/views/index.html");
+});
+
+app.get("/api/whoami", function (req, res) {
+  console.log(req.ip);
+  return res.send({
+    ...req.headers,
+    ip: req.ip,
+    software: req.headers["user-agent"],
+    language: req.headers["accept-language"],
+  });
+});
+
+if (!process.env.DETA_RUNTIME) {
+  const listener = app.listen(process.env.PORT ?? 3000, function () {
+    console.log(
+      "Your app is listening on http://localhost:" + listener.address().port
+    );
+  });
+}
+
+module.exports = app;
